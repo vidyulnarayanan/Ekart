5fbee48bcbe11939454fa353bd79a8637fed43bb
vidyulnarayanan <vidyulnarayanan99@gmail.com>
2024-07-05 01:40:30 +0530

    Products page

--- Ekart/products/views.py
+++ Ekart/products/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import product
+from django.core.paginator import Paginator
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def list_products(request):
+    """
+    Returns product list page as HTML
+    """
+    page=1
+    if request.GET:
+        page=request.GET.get('page',1)
+    product_list=product.objects.all()
+    product_paginator = Paginator(product_list,10)
+    product_list=product_paginator.get_page(page)
+    context={'products':product_list}
+    return render(request, 'products.html',context)
+
+def detail_product(request):
+    return render(request, 'product_detail.html')
